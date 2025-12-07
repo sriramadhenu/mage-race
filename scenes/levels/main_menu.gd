4cebd4cece1d39edf3161fe6950597bff5b681ba
SaltyGamer829 <sriramadhenu@SriLenovo.localdomain>
2025-12-06 20:51:07 -0800

    make the level loader show up after main menu screen

--- scenes/levels/main_menu.gd
+++ scenes/levels/main_menu.gd
@@ -11,6 +11,5 @@ func _ready():
 
 
 func _on_start_pressed():
-	# Load first level when button is pressed
-	GameManager.load_specific_level(0)
+	get_tree().change_scene_to_file("res://scenes/ui/menus/level_loader.tscn")
 	queue_free()
--- scripts/ui/level_loader.gd
+++ scripts/ui/level_loader.gd
@@ -1,12 +1,26 @@
-extends Node
+extends Control
+
+func _ready():
+	Hud.hide()
+	for child in Hud.get_children():
+		child.hide()
+
+	if has_node("LoadForestLevel"):
+		$LoadForestLevel.pressed.connect(_on_load_forest_level_pressed)
+	if has_node("LoadIceLevel"):
+		$LoadIceLevel.pressed.connect(_on_load_ice_level_pressed)
+	if has_node("LoadLavaLevel"):
+		$LoadLavaLevel.pressed.connect(_on_load_lava_level_pressed)
 
 func _on_load_forest_level_pressed() -> void:
-	GameManager.load_specific_level(0)
-
+	_start_level(0)
 
 func _on_load_ice_level_pressed() -> void:
-	GameManager.load_specific_level(1)
-
+	_start_level(1)
 
 func _on_load_lava_level_pressed() -> void:
-	GameManager.load_specific_level(2)
+	_start_level(2)
+
+func _start_level(index: int) -> void:
+	GameManager.load_specific_level(index)
+	queue_free()
