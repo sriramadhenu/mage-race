88a0de40421b6761d487cefd8dea9633fd28edba
Jacob Parker <blocckba5her@gmail.com>
2025-12-06 18:50:14 -0800

    give skeletons aimbot

--- scripts/enemies/skeleton_enemy.gd
+++ scripts/enemies/skeleton_enemy.gd
@@ -76,9 +76,10 @@ func _start_attack_target():
 
 func _finish_attack_target():
 	var arrow: ArrowProjectile = _arrow_scene.instantiate()
+	arrow.inaccurate = true
 	arrow.global_position = $ArrowSpawn.global_position
 	LevelManager.current_level_scene.add_child(arrow)
-	
+
 	arrow.add_collision_exception_with(self)
 	arrow.launch_at(target.global_position)
 
