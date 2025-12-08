282601600286f5c172c23eaf129f44d2e339d454
Adam Lee <daddamlee@gmail.com>
2025-12-07 19:32:22 -0800

    fixed hud not showing up

--- scripts/managers/level_manager.gd
+++ scripts/managers/level_manager.gd
@@ -1,25 +1,17 @@
 extends Node
 
-
 const LEVELS = [
 	"res://scenes/levels/level_forest.tscn",
 	"res://scenes/levels/level_ice.tscn",
 	"res://scenes/levels/level_lava.tscn",
 ]
 
-
 var current_level_scene: Node = null
 var hud_scene: PackedScene = preload("res://scenes/ui/hud/player_health/player_health.tscn")
-var hud_instance: CanvasLayer = null
 
 
 func _ready():
 	GameManager.level_changed.connect(_on_level_changed)
-
-
-func _on_level_button_pressed(level_index):
-	GameManager.load_specific_level(level_index)
-	queue_free()  
 
 
 func _on_level_changed(level_index: int):
@@ -27,37 +19,35 @@ func _on_level_changed(level_index: int):
 
 
 func _ensure_hud():
-	if hud_instance == null:
-		hud_instance = hud_scene.instantiate()
-		get_tree().root.add_child.call_deferred(hud_instance)
+	if Engine.has_singleton("Hud"):
+		if Hud.visible == false:
+			Hud.show()
+		return
+
+	var new_hud = hud_scene.instantiate()
+	get_tree().root.add_child.call_deferred(new_hud)
 
 
+# Level management
 func load_level(level_index: int):
+	# remove current level if it exists
 	if current_level_scene:
 		current_level_scene.queue_free()
 		await current_level_scene.tree_exited
-	
-	if level_index < LEVELS.size() and level_index >= 0:
-		var level_path = LEVELS[level_index]
-		var level_scene = load(level_path)
-		
-		if level_scene:
-			_ensure_hud()
-			hud_instance.visible = true
-			current_level_scene = level_scene.instantiate()
+
+	# Load new level
+	if level_index >= 0 and level_index < LEVELS.size():
+		var level_res = load(LEVELS[level_index])
+		if level_res:
+			_ensure_hud()       
+			Hud.visible = true    
+			current_level_scene = level_res.instantiate()
 			get_tree().root.add_child.call_deferred(current_level_scene)
 		else:
-			print("Could not load level from: ", level_path)
+			print("Could not load level at: ", LEVELS[level_index])
 	else:
-		if hud_instance:
-			hud_instance.visible = false
+		Hud.visible = false
 		GameManager.change_state(GameManager.GameState.GAME_OVER)
-
-
-func unload_level():
-	if current_level_scene:
-		current_level_scene.queue_free()
-		current_level_scene = null
 
 
 func restart_current_level():
