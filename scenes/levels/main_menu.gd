1286a24eec6786c4c710dfbafbab7f3d16a625d3
Adam Lee <daddamlee@gmail.com>
2025-12-10 17:04:03 -0800

    main menu refinement

--- scenes/levels/main_menu.gd
+++ scenes/levels/main_menu.gd
@@ -4,29 +4,32 @@ func _ready():
 	await get_tree().process_frame
 
 	# Connect Start button
-	if has_node("ButtonsContainer/StartButton"):
-		$ButtonsContainer/StartButton.pressed.connect(_on_start_pressed)
+	if has_node("UIContainer/StartButton"):
+		$UIContainer/StartButton.pressed.connect(_on_start_pressed)
 
 	# Connect Quit button
-	if has_node("ButtonsContainer/QuitButton"):
-		$ButtonsContainer/QuitButton.pressed.connect(_on_quit_pressed)
+	if has_node("UIContainer/QuitButton"):
+		$UIContainer/QuitButton.pressed.connect(_on_quit_pressed)
 
-	Hud.hide() # quick fix to hide hud in main menu for now
+	# Hide HUD for the menu
+	Hud.hide()
 	for child in Hud.get_children():
 		child.hide()	
 	
+	# Play menu music
 	$Audio/menu_music.play()
+
+	# Stop level music if present
 	if LevelMusic:
 		LevelMusic.stop()
-	
+
+
 func _on_start_pressed():
 	$Audio/start_button.play()
 	$Audio/menu_music.stop()
 	await get_tree().create_timer(0.4).timeout
 	get_tree().change_scene_to_file("res://scenes/ui/level_loader.tscn")
-	#queue_free()
 
 
 func _on_quit_pressed():
 	get_tree().quit()
-	call_deferred("queue_free")
